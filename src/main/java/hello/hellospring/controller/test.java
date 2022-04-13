db59d1f433c3fc02094028c1c87f9fc0fc1ce617
iniline <1004dlagywls@naver.com>
2022-04-14 08:56:15 +0900

    스프링 데이터 JPA , AOP 적용

---
+++ src/main/java/hello/hellospring/controller/test.java
@@ -0,0 +1,11 @@
+package hello.hellospring.controller;
+
+public class test {
+    public static void main(String[] args) {
+        int v1=15;
+        int v2 =0;
+        v2= v1-10;
+        int v3 = v1+v2+5;
+        System.out.println(v3);
+    }
+}
